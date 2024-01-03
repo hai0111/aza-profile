086be441821c589196f27930bb70d2072179c858
aza <nguyenhai011101@gmail.com>
2024-01-04 01:23:44 +0700

    minor

--- components/animations/FadeUp.tsx
+++ components/animations/FadeUp.tsx
@@ -1,27 +1,28 @@
-import { motion } from "framer-motion";
-import { FC, ReactNode } from "react";
+import { motion } from 'framer-motion'
+import { FC, ReactNode } from 'react'
 
 interface Props {
-  children: ReactNode;
-  delay?: number;
-  duration?: number;
-  onAnimationStart?(): void;
+	children: ReactNode
+	delay?: number
+	duration?: number
+	onAnimationStart?(): void
 }
 
 const FadeUp: FC<Props> = ({
-  children,
-  delay = 0,
-  duration = 0.25,
-  onAnimationStart,
+	children,
+	delay = 0,
+	duration = 0.25,
+	onAnimationStart,
 }) => (
-  <motion.div
-    initial={{ y: 20, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration, delay }}
-    onAnimationStart={onAnimationStart}
-  >
-    {children}
-  </motion.div>
-);
+	<motion.div
+		initial={{ y: 20, opacity: 0 }}
+		animate={{ y: 0, opacity: 1 }}
+		exit={{ y: 20, opacity: 0 }}
+		onAnimationStart={onAnimationStart}
+		transition={{ duration, delay }}
+	>
+		{children}
+	</motion.div>
+)
 
-export default FadeUp;
+export default FadeUp

20ac28882d8391a5f7bee5a666b16136cdb80d99
aza <nguyenhai011101@gmail.com>
2024-01-19 16:31:59 +0700

    fix bugs: types and dependencies

--- ckeditor5/src/ckeditor.ts
+++ ckeditor5/src/ckeditor.ts
@@ -3,29 +3,34 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic'
 
-import { Alignment } from '@ckeditor/ckeditor5-alignment';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Alignment } from '@ckeditor/ckeditor5-alignment'
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat'
 import {
 	Bold,
 	Code,
 	Italic,
 	Subscript,
 	Superscript,
-	Underline
-} from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { CKBox } from '@ckeditor/ckeditor5-ckbox';
-import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
-import type { EditorConfig } from '@ckeditor/ckeditor5-core';
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
-import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
-import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
+	Underline,
+} from '@ckeditor/ckeditor5-basic-styles'
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote'
+import { CKBox } from '@ckeditor/ckeditor5-ckbox'
+import { CloudServices } from '@ckeditor/ckeditor5-cloud-services'
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block'
+import type { EditorConfig } from '@ckeditor/ckeditor5-core'
+import { Essentials } from '@ckeditor/ckeditor5-essentials'
+import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace'
+import {
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+} from '@ckeditor/ckeditor5-font'
+import { Heading } from '@ckeditor/ckeditor5-heading'
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line'
+import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed'
 import {
 	Image,
 	ImageCaption,
@@ -34,24 +39,24 @@ import {
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
-	PictureEditing
-} from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link';
-import { List, TodoList } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
-import { PageBreak } from '@ckeditor/ckeditor5-page-break';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+	PictureEditing,
+} from '@ckeditor/ckeditor5-image'
+import { Indent } from '@ckeditor/ckeditor5-indent'
+import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link'
+import { List, TodoList } from '@ckeditor/ckeditor5-list'
+import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed'
+import { PageBreak } from '@ckeditor/ckeditor5-page-break'
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph'
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office'
 import {
 	Table,
 	TableCaption,
 	TableCellProperties,
 	TableProperties,
-	TableToolbar
-} from '@ckeditor/ckeditor5-table';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
-import { Undo } from '@ckeditor/ckeditor5-undo';
+	TableToolbar,
+} from '@ckeditor/ckeditor5-table'
+import { TextTransformation } from '@ckeditor/ckeditor5-typing'
+import { Undo } from '@ckeditor/ckeditor5-undo'
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
 // See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
@@ -104,8 +109,8 @@ class Editor extends ClassicEditor {
 		TextTransformation,
 		TodoList,
 		Underline,
-		Undo
-	];
+		Undo,
+	]
 
 	public static override defaultConfig: EditorConfig = {
 		toolbar: {
@@ -148,8 +153,8 @@ class Editor extends ClassicEditor {
 				'insertTable',
 				'horizontalLine',
 				'findAndReplace',
-				'pageBreak'
-			]
+				'pageBreak',
+			],
 		},
 		language: 'en',
 		image: {
@@ -159,8 +164,8 @@ class Editor extends ClassicEditor {
 				'imageStyle:inline',
 				'imageStyle:block',
 				'imageStyle:side',
-				'linkImage'
-			]
+				'linkImage',
+			],
 		},
 		table: {
 			contentToolbar: [
@@ -168,10 +173,10 @@ class Editor extends ClassicEditor {
 				'tableRow',
 				'mergeTableCells',
 				'tableCellProperties',
-				'tableProperties'
-			]
-		}
-	};
+				'tableProperties',
+			],
+		},
+	} as any
 }
 
-export default Editor;
+export default Editor
